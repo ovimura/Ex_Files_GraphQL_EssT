ce7a38dc6d39fa7414d419ed6d13ac6366e23e7b
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-06-01 23:30:42 -0700

    adding comments code and exercices code for CH 01, section 04

--- Exercise Files/CH01/01_04/begin/index.js
+++ Exercise Files/CH01/01_04/begin/index.js
@@ -1,9 +1,23 @@
+// Doc ref: https://github.com/graphql/express-graphql
+
 import express from 'express';
+import schema from './schema';
+
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('Graphql is amazing!');
 });
+
+const root = {hello: () => "Hi, I'm Manny"};
+
+app.use('/graphql', graphqlHTTP ({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    }),
+);
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
