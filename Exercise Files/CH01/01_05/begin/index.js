78dedbe0bec07a2f66f18a3357234027d9cc79c3
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-06-04 17:16:38 -0700

    adding comments Ch01 and section 5

--- Exercise Files/CH01/01_05/begin/index.js
+++ Exercise Files/CH01/01_05/begin/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import schema from './schema';
-import { graphqlHTTP } from 'express-graphql';
+//import { graphqlHTTP } from 'express-graphql';
+
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
@@ -8,7 +10,15 @@ app.get('/', (req,res) => {
     res.send('Graphql is amazing!');
 });
 
-const root = { hello: () => "Hi, I'm Manny"};
+const root = { friend: () => {
+    return {
+        "id": 654654654654654,
+        "firstName": "Manny",
+        "lastName": "Henri",
+        "gender": "Male",
+        "email": "me@me.com"
+    }
+}};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
